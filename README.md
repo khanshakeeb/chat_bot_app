c8ea037db3030c5fa6c6fd07be1492be5f686670
khanshakeeb <khanshakeeb@msn.com>
2021-04-16 16:15:46 +0200

    README and api collection export

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # chat_bot_app
 It is messenger kind of app which deals with APIs
+
+## development
+`npm run watch`
+
+## production mode
+`npm run start`
+
+## API endpoints
+There is a postman collection exported to root of directory with the name of API_collection
+
+## Configuration
+There is folder called `config` here is DB configurations
+
+## Installation
+`npm i `
--- src/controllers/messenger.controller.js
+++ src/controllers/messenger.controller.js
@@ -1,49 +1,60 @@
 import chatBotDB from '../services/chatBotDB.service';
 import bot from '../services/bot.service';
+import {APP_CONST, STATUS_CODE} from '../utils';
 
 class MessengerController {
     async connect(req, res) {
-        const {body} = req;
-        const {conversation = {}, messages = []} = await chatBotDB.createConversation(body);
-        const {intents = []} = await bot.sendIndent({
-            conversationId: conversation._id,
-            message: body.message,
-        });
-        const message = await chatBotDB.createMessage([{
-            conversationId: conversation._id,
-            intents
-        }]);
+        try {
+            const {body} = req;
+            const {conversation = {}, messages = []} = await chatBotDB.createConversation(body);
+            const {intents = []} = await bot.sendIndent({
+                conversationId: conversation._id,
+                message: body.message,
+            });
+            const message = await chatBotDB.createMessage([{
+                conversationId: conversation._id,
+                intents
+            }]);
 
-        messages.push(message);
-        res.json({
-            conversationId: conversation._id,
-            messages
-        });
+            messages.push(message);
+            res.json({
+                conversationId: conversation._id,
+                messages
+            }, STATUS_CODE.SUCCESS);
+        } catch (error) {
+            res.json(error, STATUS_CODE.BAD_REQUEST);
+        }
+
     }
 
     async send(req, res) {
-        const {body} = req;
-        const {intents = []} = await bot.sendIndent({
-            conversationId: body.conversationId,
-            message: body.message
-        });
-        const sendData = [
-            {
+        try {
+            const {body} = req;
+            const {intents = []} = await bot.sendIndent({
                 conversationId: body.conversationId,
-                messageBy: 'User',
                 message: body.message
-            },
-            {
+            });
+            const sendData = [
+                {
+                    conversationId: body.conversationId,
+                    messageBy: APP_CONST.MESSAGE_BY_HUMAN,
+                    message: body.message
+                },
+                {
+                    conversationId: body.conversationId,
+                    messageBy: APP_CONST.MESSAGE_BY_BOT,
+                    intents
+                }
+            ];
+            const messages = await chatBotDB.createMessage(sendData);
+            res.json({
                 conversationId: body.conversationId,
-                messageBy: 'Bot',
-                intents
-            }
-        ];
-        const messages = await chatBotDB.createMessage(sendData);
-        res.json({
-            conversationId: body.conversationId,
-            messages
-        });
+                messages
+            }, STATUS_CODE.SUCCESS);
+        } catch (error) {
+            res.json(error, STATUS_CODE.BAD_REQUEST);
+        }
+
     }
 }
 
